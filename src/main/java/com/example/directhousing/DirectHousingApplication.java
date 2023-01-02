a6d5c5fc28fc60603489f2f91bd9eb0a56afc326
Dong <rlaehddnr1009@naver.com>
2023-01-02 19:18:53 +0900

    init Spring boot

---
+++ src/main/java/com/example/directhousing/DirectHousingApplication.java
@@ -0,0 +1,13 @@
+package com.example.directhousing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DirectHousingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DirectHousingApplication.class, args);
+    }
+
+}
